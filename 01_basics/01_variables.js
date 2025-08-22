7670d946c089e8ea22c3d7d501a8c76f06a2a4bb
MD YASIN ARAFAT <mdyasinarafat1712@gmail.com>
2025-08-22 18:36:24 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "mdyasinarafat1712@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mohammadpur"
+
+//  accountId = 2 // not allowed
+
+
+accountEmail = "hk@gmail.com"
+accountPassword = "5"
+accountCity = "bay"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+/*
+Prefer not use var
+becuase of issue in block scope and functional scope
+*/
---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "arafat"
+let age = 16
+
+
+// nuumber => 2 to power 35
+// bigint =>
+// string => ""
+// boolean => true/false
+// null => standalne value
+// undefiened => 
+// symbol => uniquness
+
+
+
+
+// object
+
+console.log(typeof "null"); // object
