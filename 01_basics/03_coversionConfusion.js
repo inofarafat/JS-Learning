eefe6f74e2283ca9859cac69bd91972533d33daf
MD YASIN ARAFAT <mdyasinarafat1712@gmail.com>
2025-08-24 18:40:53 +0000

    chnge

--- 01_basics/03_coversionConfusion.js
+++ 01_basics/03_coversionConfusion.js
@@ -1,6 +1,11 @@
-let score = "33"
+let score = '33';
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber =  Number(score)
-console.log(typeof valueInNumber);
+// operationssssssssss
+
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
